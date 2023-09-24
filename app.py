5e4a7b796d2313b8e20b507511cb5a633bea17ff
GURNANI YUKTA RAJKUMAR <143731993+Yukta026@users.noreply.github.com>
2023-09-24 22:16:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 import os
 import pickle
 import joblib
@@ -43,27 +43,6 @@ def code():
 def recommend_ui():
     return render_template('recommend.html')
 
-
-@app.route('/recommend_books',methods=['post'])
-def recommend():
-    user_input = request.form.get('user_input')
-    index = np.where(pt.index == user_input)[0][0]
-    similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:6]
-    data = []
-    for i in similar_items:
-        item = []
-        #         print(pt.index[i[0]])
-        temp_df = books[books['Book-Title'] == pt.index[i[0]]]
-        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
-        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
-        item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
-
-        data.append(item)
-    print(data)
-
-    return render_template('recommend.html',data=data)
-    
-   
 if __name__ == '__main__':
     app.run(debug=True)
 
